c839f5e968983101b7f66271b6969567a803ba0f
Rinat <mochen703@gmail.com>
2023-08-07 22:25:32 -0400

    fined Bora automation

--- src/test/java/selenium/CreatBoratechProfile.java
+++ src/test/java/selenium/CreatBoratechProfile.java
@@ -1,11 +1,23 @@
 package selenium;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CreatBoratechProfile {
 	public static void main(String[] args) {
+		String jobTitle = "QA Tester";
+		String companeyName = "Target" + getTimestamps();
+		String location = "Maryland";
+		String from = "02/01/2023";
+		String to = "08/01/2023";
+		String describtion = "do automation ";
+		boolean current = true;
 		WebDriver driver = new ChromeDriver();
 		try {
 			driver.get("https://boratech-practice-app.onrender.com/dashboard");
@@ -14,27 +26,44 @@ public class CreatBoratechProfile {
 			driver.findElement(By.xpath("//*[@id=\"root\"]/section/form/input")).click();
 			Thread.sleep(2000);
 			driver.findElement(By.xpath("//a[@href=\"/add-experience\"]")).click();
-			driver.findElement(By.name("title")).sendKeys("QA Tester");
-			driver.findElement(By.name("company")).sendKeys("Puri digit");
-			driver.findElement(By.name("location")).sendKeys("Maryland");
-			driver.findElement(By.name("from")).sendKeys("02/01/2023");
-			driver.findElement(By.name("to")).sendKeys("08/01/2023");
+			driver.findElement(By.name("title")).sendKeys(jobTitle);
+			driver.findElement(By.name("company")).sendKeys(companeyName);
+			driver.findElement(By.name("location")).sendKeys(location);
+			driver.findElement(By.name("from")).sendKeys(from);
+			if (current) {
+				driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+			} else {
+				driver.findElement(By.name("to")).sendKeys(to);
+			}
+
+			driver.findElement(By.tagName("textarea")).sendKeys(describtion);
 			driver.findElement(By.xpath("//input[@type=\"submit\"]")).click();
-			/*
-			 * WebElement descripbox = driver.findElement(By.name("description"));
-			 * descripbox.click();
-			 * 
-			 * descripbox.sendKeys(" im here because i want to inprof myself");
-			 * 
-			 */
-			Thread.sleep(2000);
+			Thread.sleep(3000);
+
 			String Title = driver.findElement(By.xpath("//*[@id=\"root\"]/section/h1")).getText();
 			if (Title.equals("Dashboard")) {
-				System.out.println("Test passed");
 			} else {
 				throw new Exception("Title does match expacted Title is Dashboard VS actral " + Title);
 			}
-			driver.findElement(By.xpath("//*[@id=\"root\"]/section/table[1]/tbody/tr/td[4]/button"));
+
+			String tableElement = "//h2[text()='Experience Credentials']/following-sibling::table[1]";
+			String rolElement = tableElement + "/tbody/tr";
+			WebElement table = driver.findElement(By.xpath(tableElement));
+			List<WebElement> rols = table.findElements(By.xpath(rolElement));
+			boolean found = false;
+			for (WebElement rol : rols) {
+				List<WebElement> cells = rol.findElements(By.tagName("td"));
+				String actualCompany = cells.get(0).getText();
+				String actualTile = cells.get(1).getText();
+				if (actualCompany.equals(companeyName) && actualTile.equals(jobTitle)) {
+					found = true;
+					break;
+
+				}
+				if (found == false) {
+					throw new Exception("The newly input can not be found");
+				}
+			}
 
 			System.out.println("Test Passed");
 
@@ -45,6 +74,16 @@ public class CreatBoratechProfile {
 			// driver.quit();
 		}
 
+	}
+
+	public static void wait(int second) throws InterruptedException {
+		Thread.sleep(second * 1000);
+	}
+
+	public static String getTimestamps() {
+
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		return timestamp.getTime() + "";
 	}
 
 }
