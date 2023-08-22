dfb7fd7e7f2c30a87a9414a4ed7ffbbb8f80f09c
Rinat <mochen703@gmail.com>
2023-08-21 23:29:42 -0400

    education api update

--- src/test/java/api/BoraTechEddEducationApi.java
+++ src/test/java/api/BoraTechEddEducationApi.java
@@ -1,11 +1,14 @@
 package api;
 
-import com.google.gson.JsonObject;
+import java.util.List;
 
+import apiPojos.Education;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import utilities.BoraTechApi;
+import utilities.Keywords;
 
 public class BoraTechEddEducationApi {
 
@@ -15,19 +18,26 @@ public class BoraTechEddEducationApi {
 		RequestSpecification request = RestAssured.given();
 		request.header("Content-Type", "application/json");
 		request.header("x-auth-token", token);
-		JsonObject body = new JsonObject();
-		body.addProperty("school", "harvord001");
-		body.addProperty("degree", "QA cetified");
-		body.addProperty("fieldofstudy", "Automation");
-		body.addProperty("from", "2023-08-16");
-		body.addProperty("to", "");
-		body.addProperty("current", true);
-		body.addProperty("description", "");
+		Education body = new Education("MSN001"+Keywords.getTimeStamp(), "QA certified", "Automation", "2013-08-16", "2018-09-01", false,
+				"Study hard everday");
 		request.body(body);
 		Response response = request.put("api/profile/education");
-		String result = response.getStatusLine();
-		System.out.println(result);
-		System.out.println(response.getBody().asPrettyString());
+
+		JsonPath jp = response.jsonPath();
+		List<Education> educations = jp.getList("education", Education.class);
+		boolean found = false;
+		for (Education education : educations) {
+			if(education.school.equals(body.school)&&education.degree.equals(body.degree)) {
+				found=true;
+				break;
+			}
+		}
+	
+		if(found==true) {
+			System.out.println("Passed");
+		}else {
+			System.out.println("Feild");
+		}
 
 	}
 
